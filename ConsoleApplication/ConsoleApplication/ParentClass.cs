3975946ace3441ea81e5c9d6e7fda3db3dcd8869
pragneshak <kulkarni.pragnesha@gmail.com>
2024-02-11 12:50:30 +0530

    Method overriding and method hiding

---
+++ ConsoleApplication/ConsoleApplication/ParentClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ConsoleApplication
+{
+	public class ParentClass
+	{
+		public virtual void Show()
+		{
+			Console.WriteLine("Parent class Show Method");
+		}
+        public void Display()
+        {
+            Console.WriteLine("Parent Class Display Method");
+        }
+    }
+    public class Child : ParentClass
+    {
+        //Method Overriding
+        //In Method Overriding, the child classes re-implement their parent class methods which are declared as virtual.
+        //That means here, the child classes re-implement the parent class methods with the permission of the parent class because
+        //here in the parent class the method is declared as virtual giving permission to the child classes for overriding the methods
+        //using the override modifier.
+
+        public override void Show()
+        {
+            Console.WriteLine("Child Class Show Method");
+        }
+        //reimplementation of virtual method is also possible with new keyword i.e. Method Hiding.
+        //public new void Show()
+        //{
+        //    Console.WriteLine("Child2 Class Show Method");
+        //}
+        //Method Hiding/Shadowing
+        //In Method Hiding/Shadowing, the child classes can re-implement any method of its parent class methods even
+        //if they are not declared as virtual.
+        //That means here the child class re-implements the parent class methods without taking any permission from its parent.
+
+        public new void Display()
+        {
+            Console.WriteLine("Child Class Display Method");
+        }
+    }
+}
+
--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -4,11 +4,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Calculator calObj = new Calculator();
-        int result = calObj.Calculatesum(10, 20);
-        Console.WriteLine(result);
+        //Calculator calObj = new Calculator();
+        //int result = calObj.Calculatesum(10, 20);
+        //Console.WriteLine(result);
+        //Console.ReadKey();
+
+        //For method overriding and method hiding
+        Child obj = new Child();
+        obj.Show();
+        obj.Display();
         Console.ReadKey();
-       
     }
 }
 public class Calculator
